ca48bc82759292c3441d19ad936539b41fc189d5
kalina235 <46924011+kalina235@users.noreply.github.com>
2021-12-10 23:32:02 +0000

    Add files via upload

---
+++ MCSP.cpp
@@ -0,0 +1,120 @@
+#include<bits/stdc++.h>
+#include"MCSP.h"
+#include<stdio.h>
+
+using namespace std;
+
+void MCSP::GenerateLaplacian() 
+{
+	auto deg  = VectorXd(m_matSize);
+	for(int i = 0; i < m_matSize;i++)
+	{
+		for(int j = 0; j < m_matSize; j++)
+			{
+				if(m_classes(i,j) > 0.0000001 && i != j) 
+				{
+					deg(i) += m_classes(i,j);//1
+					deg(j) += m_classes(i,j);//1
+				}
+			}
+	}
+	m_laplacian=MatrixXd(m_matSize,m_matSize);
+	for(int i = 0; i < m_matSize;i++)
+	{
+		for(int j = 0; j < m_matSize; j++)
+			{
+				if(m_classes(i,j) > 0.0000001 && i != j) 
+				{
+					m_laplacian(i,j) = -2.0/sqrt(deg(i)*deg(j));//1
+				}
+				if(i == j)
+				{
+					m_laplacian(i,j) = 1.0;
+				}
+			}
+	}
+}
+void MCSP::FindCoordinates()
+{
+	SelfAdjointEigenSolver<MatrixXd> eigenvaluesGen(m_laplacian);
+	cout << eigenvaluesGen.eigenvalues();
+	VectorXd eigenvalues = eigenvaluesGen.eigenvalues();
+	auto min1 = make_pair(HUGE_VAL, 0);
+	auto min2 = make_pair(HUGE_VAL, 0);
+	
+	for(auto index = 0; index < eigenvalues.size(); index++)
+	{
+		if(eigenvalues(index) <= 0.0000001)
+		{ 
+			continue;
+		}
+		if(eigenvalues(index) <= min1.first) 
+		{
+			min2 = min1;
+			min1 = make_pair(eigenvalues(index), index);
+			continue;
+		}
+		if(eigenvalues(index) <= min2.first) 
+		{
+			min2 = make_pair(eigenvalues(index), index);
+		}
+	}
+	
+	auto xCoordinates = eigenvaluesGen.eigenvectors().col(1);
+	auto yCoordinates = eigenvaluesGen.eigenvectors().col(2);
+	for(auto index = 0; index < xCoordinates.size(); index++)
+	{
+		m_finalCoordinates.push_back(make_pair(xCoordinates(index),yCoordinates(index)));
+	}
+}
+
+void MCSP::PrintCoordinates(std::string fileName)
+{
+	freopen(fileName.c_str(), "w", stdout);
+	printf("{%lf,%lf}", m_finalCoordinates[0].first, m_finalCoordinates[0].second);
+	for(auto index = 1; index < m_matSize; index++)
+	{
+		printf(",{%lf,%lf}", m_finalCoordinates[index].first, m_finalCoordinates[index].second);
+	}
+	fclose(stdout);
+}
+
+void MCSP::PrintAdjacency( std::string fileName )
+{
+	freopen(fileName.c_str(),"w",stdout);
+	for(int i=0;i<m_matSize;i++)
+	{
+		printf("{");
+		printf("%lf", m_classes(i,0));
+		for(int j=1;j<m_matSize;j++)
+		{
+			printf(",%lf", m_classes(i,j));
+		}
+		printf("}\n");
+		if(i!=m_matSize-1) 
+		{
+			printf(",");
+		}
+	}
+		
+}
+
+MCSP::MCSP(std::string fileName, int matSize):
+	m_matSize(matSize),
+	m_classes(MatrixXd(matSize,matSize))	
+{
+	freopen(fileName.c_str(), "r", stdin);
+	
+	for(auto i = 0; i < m_matSize; i++)
+	{
+		for(auto j = 0; j < m_matSize; j++)
+		{
+			double x;
+			scanf("%lf", &x);
+			m_classes(i,j) = x;
+		}
+	}
+	fclose(stdin);
+	GenerateLaplacian();
+	FindCoordinates();
+}
